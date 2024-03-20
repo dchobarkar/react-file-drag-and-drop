0195a9c1d74941a492e90b14f55a2b4c0b9b0ca1
Darshan Chobarkar <dchobarkar@gmail.com>
2024-03-20 14:17:48 +0530

    display image gallery

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,19 @@ import { DragAndDropPad } from "./components/drag-and-drop";
 import "./App.css";
 import { useState } from "react";
 import { FileList } from "./components/file-list";
+import { Gallery } from "./components/gallery";
 
 function App() {
   const [files, setFiles] = useState<Array<File>>([]);
+  const [imageUrls, setImageUrls] = useState<Array<string>>([]);
 
-  function onFileDropped(files: Array<File>) {
-    setFiles(files);
+  function onFileDropped(newFiles: Array<File>) {
+    setFiles(newFiles);
+    const urls = newFiles
+      .filter((file) => file.type === "image/jpeg")
+      .map((file) => URL.createObjectURL(file));
+
+    setImageUrls(urls);
   }
 
   return (
@@ -16,6 +23,7 @@ function App() {
         <p className="drop-hint">Drop files here</p>
       </DragAndDropPad>
       <FileList files={files} />
+      <Gallery imageUrls={imageUrls} />
     </div>
   );
 }
