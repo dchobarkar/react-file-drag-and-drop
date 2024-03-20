d6597f25a6aa06d17244a63c651eaffa11b678a6
Darshan Chobarkar <dchobarkar@gmail.com>
2024-03-20 14:12:39 +0530

    display file list

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import { DragAndDropPad } from "./components/drag-and-drop";
 import "./App.css";
+import { useState } from "react";
+import { FileList } from "./components/file-list";
 
 function App() {
+  const [files, setFiles] = useState<Array<File>>([]);
+
+  function onFileDropped(files: Array<File>) {
+    setFiles(files);
+  }
+
   return (
     <div className="app">
-      <DragAndDropPad className="drag-and-drop">
+      <DragAndDropPad className="drag-and-drop" onFileDropped={onFileDropped}>
         <p className="drop-hint">Drop files here</p>
       </DragAndDropPad>
+      <FileList files={files} />
     </div>
   );
 }
