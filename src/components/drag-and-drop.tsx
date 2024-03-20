07f0afaaeb032c42a8651323f080b5d0387cee68
Darshan Chobarkar <dchobarkar@gmail.com>
2024-03-20 14:07:52 +0530

    process drop event

--- src/components/drag-and-drop.tsx
+++ src/components/drag-and-drop.tsx
@@ -2,15 +2,35 @@ import { DragEvent, PropsWithChildren } from "react";
 
 type DragAndDropPadProps = {
   className?: string;
+  onFileDropped: (files: Array<File>) => void;
 };
 
 export const DragAndDropPad = ({
   className,
   children,
+  onFileDropped,
 }: PropsWithChildren<DragAndDropPadProps>) => {
   function onDrop(event: DragEvent<HTMLDivElement>) {
     event.preventDefault();
-    console.log(event);
+
+    const items = event.dataTransfer.items;
+    const files = event.dataTransfer.files;
+
+    if (items) processItems([...items]);
+    else processFiles([...files]);
+  }
+
+  function processItems(items: Array<DataTransferItem>) {
+    const files = items
+      .filter((item) => item.kind === "file")
+      .map((item) => item.getAsFile())
+      .filter((file): file is File => file !== null);
+
+    processFiles(files);
+  }
+
+  function processFiles(files: Array<File>) {
+    onFileDropped(files);
   }
 
   function onDragOver(event: DragEvent<HTMLDivElement>) {
